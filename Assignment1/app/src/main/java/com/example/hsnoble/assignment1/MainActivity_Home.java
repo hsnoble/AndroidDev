d1be32c2c1282ba30b1474058ee33d9776269fb5
Stephen Noble <stephen.noble0514@gmail.com>
2018-02-12 14:54:29 -0500

    Added logic skeleton

--- Assignment1/app/src/main/java/com/example/hsnoble/assignment1/MainActivity_Home.java
+++ Assignment1/app/src/main/java/com/example/hsnoble/assignment1/MainActivity_Home.java
@@ -1,7 +1,10 @@
 package com.example.hsnoble.assignment1;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity_Home extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity_Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__home);
+        //Prepare buttons
+        final Button hi = findViewById(R.id.say_hi);
+        final Button hello = findViewById(R.id.say_hello);
+        //Prepare Radio Groups
+        RadioGroup hi_group = (RadioGroup) findViewById(R.id.hi_radio);
+        RadioGroup hello_group = (RadioGroup) findViewById(R.id.hello_radio);
+
+        hi_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                //for hi
+            }
+        });
+        hello_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedID) {
+                //for hello
+            }
+        });
     }
 }
