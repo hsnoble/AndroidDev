8db3c35967ff582fcf7e2f72b3e6c7f8b32b4153
Stephen Noble <stephen.noble0514@gmail.com>
2018-04-09 04:32:40 -0400

    Implemented data getter

--- A5/app/src/main/java/com/example/hsnoble/assignment5/MainActivity.java
+++ A5/app/src/main/java/com/example/hsnoble/assignment5/MainActivity.java
@@ -3,11 +3,52 @@ package com.example.hsnoble.assignment5;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> arr = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get data from JSON
+        getData();
+    }
+
+    public void getData()
+    {
+        String jsonData;
+        try
+        {
+            InputStream is = getAssets().open("data.json");
+            int sz = is.available();
+            byte[] buffer = new byte[sz];
+            is.read();
+            is.close();
+
+            jsonData = new String(buffer, "UTF-8");
+            JSONArray jsonArray = new JSONArray(jsonData);
+
+            /*
+            for (int x; x < jsonArray.length(); x++)
+            {
+                JSONObject obj = jsonArray.getJSONObject(x);
+                arr.add()
+            }*/
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
