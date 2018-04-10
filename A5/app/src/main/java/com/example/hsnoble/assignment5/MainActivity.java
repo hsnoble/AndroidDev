a1629b1c8d1f4a70c72e8344da24b49c83588df0
Stephen Noble <stephen.noble0514@gmail.com>
2018-04-10 03:27:28 -0400

    Implemented more stuff...I'm gonna die

--- A5/app/src/main/java/com/example/hsnoble/assignment5/MainActivity.java
+++ A5/app/src/main/java/com/example/hsnoble/assignment5/MainActivity.java
@@ -1,54 +1,107 @@
 package com.example.hsnoble.assignment5;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> arr = new ArrayList<>();
+    String dataLink = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&endtime=2018-12-31&minmagnitude=6";
+    ListView earthquakeList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //get data from JSON
-        getData();
+        EarthquakeAsync e = new EarthquakeAsync();
+        e.execute(dataLink);
     }
 
-    public void getData()
+    class EarthquakeAsync extends AsyncTask<String, Void, List<String>>
     {
-        String jsonData;
-        try
+        @Override
+        protected List<String> doInBackground(String... stringurl)
         {
-            InputStream is = getAssets().open("data.json");
-            int sz = is.available();
-            byte[] buffer = new byte[sz];
-            is.read();
-            is.close();
-
-            jsonData = new String(buffer, "UTF-8");
-            JSONArray jsonArray = new JSONArray(jsonData);
-
-            /*
-            for (int x; x < jsonArray.length(); x++)
-            {
-                JSONObject obj = jsonArray.getJSONObject(x);
-                arr.add()
-            }*/
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }catch (JSONException e)
-        {
-            e.printStackTrace();
+            return Utils.fetchEarthquakeData(stringurl[0]);
         }
+        public void onPostExecute(List<String> postExecuteResult)
+        {
+            CustomListAdapter arrayAdapter = new CustomListAdapter (MainActivity.this, postExecuteResult);
+            earthquakeList= findViewById(R.id.data);
+            earthquakeList.setAdapter(arrayAdapter);
+
+            earthquakeList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                    TextView currentURL = view.findViewById(R.id.url);
+                    Intent broswer = new Intent(Intent.ACTION_VIEW, Uri.parse(currentURL.getText().toString()));
+                    startActivity(broswer);
+
+
+                }
+            });
+        }
+    }
+
+}
+class CustomListAdapter extends ArrayAdapter<String>
+{
+    Activity context;
+    List<String> itemname1;
+
+    public CustomListAdapter (Activity activity, List<String> itemnameA)
+    {
+        super(activity, R.layout.one_quake,itemnameA );
+        this.context=activity;
+        this.itemname1=itemnameA;
+    }
+
+    public View getView(int position, View view, ViewGroup parent)
+    {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.one_quake,null,true);
+        String earthInfo[] = itemname1.get(position).split("@@");
+        TextView textInfo = rowView.findViewById(R.id.nameID);
+        textInfo.setText(earthInfo[0]);
+        if(position%2 == 0)
+        {
+            textInfo.setBackgroundColor(Color.parseColor("#00FF00"));
+        }
+        textInfo = rowView.findViewById(R.id.date);
+
+
+
+        textInfo.setText(earthInfo[1]);
+        if(position%2 == 0)
+        {
+            textInfo.setBackgroundColor(Color.parseColor("#00FF00"));
+        }
+        textInfo = rowView.findViewById(R.id.url);
+        textInfo.setText(earthInfo[2]);
+        textInfo.setVisibility(View.GONE);
+
+
+        return rowView;
+
     }
 }
---
+++ A5/app/src/main/java/com/example/hsnoble/assignment5/Utils.java
@@ -0,0 +1,137 @@
+package com.example.hsnoble.assignment5;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Stephen Noble on 4/10/2018.
+ */
+
+public class Utils {
+    public static final String LOG_TAG = Utils.class.getSimpleName();
+    public static List<String> fetchEarthquakeData(String requestUrl)
+    {
+        URL url = createUrl(requestUrl);
+        String res = null;
+        try{
+            res = httpReq(url);
+        } catch (IOException e){
+            Log.e(LOG_TAG, "Error Closing input stream", e);
+        }
+        return extractFeatureFromJson(res);
+    }
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try{
+            url = new URL(stringUrl);
+        }catch (MalformedURLException e){
+            Log.e(LOG_TAG,"Error with creating URL ", e);
+        }
+
+        return url;
+    }
+    private static String httpReq (URL url) throws IOException
+    {
+        String res ="";
+        if(url==null)
+        {
+            return res;
+        }
+
+        HttpURLConnection urlConnection= null;
+        InputStream inputStream = null;
+        try
+        {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            if(urlConnection.getResponseCode()==200){
+                inputStream = urlConnection.getInputStream();
+                res=streamReader(inputStream);
+            }else{
+                Log.e(LOG_TAG,"Error response code: "+urlConnection.getResponseCode());
+            }
+        }catch (IOException e){
+            Log.e(LOG_TAG, "Problem retriving the earthquake JSON reults.",e);
+        }finally {
+            if(urlConnection!=null){
+                urlConnection.disconnect();
+            }
+            if(inputStream!=null){
+                inputStream.close();
+            }
+        }
+        return res;
+
+    }
+    private static String streamReader(InputStream inputStream)throws IOException{
+        StringBuffer output = new StringBuffer();
+        if(inputStream != null)
+        {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line!=null){
+                output.append(line);
+                line=reader.readLine();
+            }
+
+        }
+        return output.toString();
+    }
+    private static List<String> extractFeatureFromJson(String earthquakeJSON){
+        if(TextUtils.isEmpty(earthquakeJSON)){
+            return null;
+        }
+        List<String> quakeList = new ArrayList<>();
+        //List<String> urls = new ArrayList<>();
+        try{
+            JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+            JSONArray earthquakeArray = baseJsonResponse.getJSONArray("features");
+            for(int i=0;i<earthquakeArray.length();i++){
+                JSONObject currentEarthquake = earthquakeArray.getJSONObject(i);
+                JSONObject properties = currentEarthquake.getJSONObject("properties");
+                String title = properties.getString("title");
+                // String time = properties.getString("time");
+                Long date1 = properties.getLong("time");
+
+                Date dateObject = new Date(date1);
+                SimpleDateFormat dateFormatter = new SimpleDateFormat("EEE MMM dd  ");
+                SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm:ss z yyyy");
+                String dateToDisplay = dateFormatter.format(dateObject);
+                String timeToDisplay = timeFormatter.format(dateObject);
+                String time = dateToDisplay + " "+ timeToDisplay ;
+
+
+                String url = properties.getString("url");
+                quakeList.add(title + "@@" + time + "@@" + url);
+
+
+            }
+            return quakeList;
+        }catch (JSONException e){
+            Log.e(LOG_TAG,"Problem Parsing the earthquake JSON results", e);
+        }
+        return null;
+
+    }
+}
